93abba1af6cac24be4baa9b17d12adbf72da7b84
Rashaad <rashaad@rashaadfontenot.com>
2018-04-23 11:18:15 -0700

    Update sensor.py
    updated code

--- sensor.py
+++ sensor.py
@@ -1,21 +1,37 @@
-import sys
-from arduino import Arduino
-import time
+#!/usr/bin/env python3
+
 import serial
+from datetime import datetime
+import random
+import time
+import requests
+from azure.cosmosdb.table.tableservice import TableService
+from azure.cosmosdb.table.models import Entity
+import urllib
+from datetime import datetime
+	
 
-'''Connect to Azure IOT hub'''
-CONNECTION_STRING = "HostName=BlackSoldierFly.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=xOiEuZJhNRslxtkio3guAyoVAkGtBRKHElBvh5xuF1I=[IoTHub Connection String]"
-DEVICE_ID = "GasSensor1"
+# Connect to Azure Table service
+table_service = TableService(account_name='urbanfarmingbsf', account_key='cXEulUA2YXEq0WEkGKvhTAZ2N64BUqBQ6MNMKxhrHpd2DfqnMVqi4p+ji2CbeY9WzQFTxs+umzLKa4d1Y6hVxA==')
 
-sys.path.append('/home/pi/Downloads/Python-Arduino-Proto-API-v2-master/arduino')
+# Connect to Azure Cosmos DB
+cosmoDB_service = TableService(connection_string='DefaultEndpointsProtocol=https;AccountName=urbanfamingbsf;AccountKey=Bp31zHqd3KFn44Y3doAfXLlAZjLal8mSzjGEtRRarP0z5AfXWI8VuPfSPvq0thTc1JQvUKx07mMkQz0sF1WNVw==;TableEndpoint=https://urbanfamingbsf.table.cosmosdb.azure.com:443/;')
 
-ard = Arduino('/dev/ttyACM0', 115200)
-#digital pins that the sensors are on
-mq136 = 0
-mq137 = 1
+
+# create table
+#new_table = table_service.create_table('NewUrbanFarmTable')
+
+ser = serial.Serial('/dev/ttyACM0',9600)
+sensor_id = 'Gas_Sensor_1'
 
 while True:
-    
-    print(ard.analogRead(mq136))
-    print(ard.analogRead(mq137))
+    value = int(ser.readline().strip())
+    rowkey = random.random()
+
+    # ensure that timestamp string is formatted properly
+    now = datetime.strftime(datetime.now(), "%Y-%m-%dT%H:%M:%S%Z")
+
+    # create entity object
+    data = {'PartitionKey': sensor_id, 'RowKey': str(rowkey), 'ammonia': value, 'hydrogen_sulfide': value, 'datetime': now}
+    table_service.insert_entity('GasSensor', data)
     time.sleep(1)
